d054937e9f64bc9c37ca3975072b37e28f39a6fb
musicacristianagratis <68211258+musicacristianagratis@users.noreply.github.com>
2024-07-16 17:03:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const RADIO_NAME = '♫ Jesus es Dios de Poder ♪';
+const RADIO_NAME = '♫ Radio Música Cristiana ♪';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stream.zeno.fm/iulto7nzk0jtv';
+const URL_STREAMING = 'https://stream.zeno.fm/efy4auvgp9duv';
 
 // You can find the mount point in the Broadcast Settings.
 // To generate the Zeno Radio API link from the mount point,
@@ -9,7 +9,7 @@ const URL_STREAMING = 'https://stream.zeno.fm/iulto7nzk0jtv';
 // For example, if the mount point is 'yn65fsaurfhvv/source',
 // the API link will be 'https://api.zeno.fm/mounts/metadata/subscribe/yn65fsaurfhvv'.
 
-const url = 'https://api.zeno.fm/mounts/metadata/subscribe/iulto7nzk0jtv';
+const url = 'https://api.zeno.fm/mounts/metadata/subscribe/efy4auvgp9duv';
 
 // Visit https://api.vagalume.com.br/docs/ to get your API key
 const API_KEY = "18fe07917957c289983464588aabddfb";
